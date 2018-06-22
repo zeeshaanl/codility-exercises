923f7ada831e4d4ca74954d412aa644a8a9c779c
zeeshaanl <zeeshaan.lakdawala@happycar.de>
2018-06-22 17:24:49 +0200

    first commit

---
+++ BinaryGap.js
@@ -0,0 +1,9 @@
+// Find the longest sequence of zeros in binary representation of an integer
+
+const longestSequenceOfZeros = (number) => {
+    const binaryRepresentation = '1001000011010100111100000000';
+    const arrayOfZeroes = binaryRepresentation.split('1');
+    return arrayOfZeroes.sort().reverse()[0].length
+};
+
+longestSequenceOfZeros(4);
